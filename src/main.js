d8618c416d6da1a084e94236f28b0613f40dcc45
mathiasbergqvist <mathiasbergqvist88@gmail.com>
2016-09-07 22:59:56 +0200

    adds additional data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import movies from '../data/movies';
+import {starWarsMovies} from '../data/data';
 
-for(let movie of movies){
+for(let movie of starWarsMovies){
   document.write(movie.title);
   document.write("<br>");
 }
